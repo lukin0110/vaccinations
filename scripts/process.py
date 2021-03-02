fd0f41345f632718ac1fc5cc05f84bb7c2b84619
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2021-03-02 23:18:10 +0100

    Cleaner process script

--- scripts/process.py
+++ scripts/process.py
@@ -4,39 +4,10 @@ import pandas as pd
 import requests
 import typer
 from os import path
-from dataclasses import dataclass
 from datetime import date, datetime, timedelta
-from typing import Any, List
+from typing import Any, Dict
 
 CSV_ENDPOINT = "https://www.laatjevaccineren.be/vaccination-info/get"
-
-
-@dataclass
-class History:
-    population: int
-    total_first_dose: int
-    total_second_dose: int
-
-    @property
-    def total_minimum_one_dose(self) -> int:
-        return self.total_first_dose + self.total_second_dose
-
-
-@dataclass
-class DailyResults:
-    history_all: History
-    history_adults: History
-    per_age_population: List[int]
-    per_age_first_dose: List[int]
-    per_age_second_dose: List[int]
-
-    @property
-    def per_age_percentage_first_dose(self) -> List[float]:
-        return [round(100*v/self.per_age_population[i], 2) for i, v in enumerate(self.per_age_first_dose)]
-
-    @property
-    def per_age_percentage_second_dose(self) -> List[float]:
-        return [round(100*v/self.per_age_population[i], 2) for i, v in enumerate(self.per_age_second_dose)]
 
 
 def data_path(date_of_file: date) -> str:
@@ -45,10 +16,10 @@ def data_path(date_of_file: date) -> str:
     return path.join(data_dir, f"vaccinations_{date_of_file:%Y-%m-%d}.csv")
 
 
-def json_path() -> str:
+def json_path(municipality: str) -> str:
     """Construct absolute path of the JSON output."""
     output_dir = path.realpath(path.join(path.dirname(path.realpath(__file__)), "..", "website", "data"))
-    return path.join(output_dir, "numbers.json")
+    return path.join(output_dir, f"numbers_{municipality.lower()}.json")
 
 
 def fetch(date_to_store: date, endpoint: str = CSV_ENDPOINT) -> None:
@@ -91,9 +62,36 @@ def load_range(start_date: date, end_date: date) -> pd.DataFrame:
     return all_df
 
 
-def crunch_day(df: pd.DataFrame, municipality: str) -> DailyResults:
+def crunch_history(df: pd.DataFrame) -> Dict[str, Any]:
+    """."""
+    grouped = df.groupby("DATE", as_index=False).agg({
+        "POPULATION_NBR": sum,
+        "VACCINATED_FIRST_DOSIS_NBR": sum,
+        "VACCINATED_SECOND_DOSIS_NBR": sum
+    }).sort_values(by="DATE", ascending=True)
+    grouped["VACCINATED_ONE_DOSIS_NBR"] = grouped.apply(
+        lambda r: r["VACCINATED_FIRST_DOSIS_NBR"] + r["VACCINATED_SECOND_DOSIS_NBR"], axis=1)
+
+    last_date = pd.Timestamp(sorted(df["DATE"].unique(), reverse=True)[0])
+    last_df = df[df["DATE"] == last_date]
+    population = int(last_df["POPULATION_NBR"].fillna(0).sum())
+    total_first_dose = int(last_df["VACCINATED_FIRST_DOSIS_NBR"].fillna(0).sum())
+    total_second_dose = int(last_df["VACCINATED_SECOND_DOSIS_NBR"].fillna(0).sum())
+
+    return {
+        "population": population,
+        "minimum_one_dose": total_first_dose + total_second_dose,
+        "first_dose": total_first_dose,
+        "second_dose": total_second_dose,
+        "timeseries_minimum_one_dose": [0] + grouped["VACCINATED_ONE_DOSIS_NBR"].apply(
+            lambda v: round(v / population * 100, 2)).tolist(),
+        "timeseries_second_dose": [0] + grouped["VACCINATED_SECOND_DOSIS_NBR"].apply(
+            lambda v: round(v / population * 100, 2)).tolist()
+    }
+
+
+def crunch_per_age(df: pd.DataFrame) -> Dict[str, Any]:
     """Compute numbers per municipality."""
-    mdf = df[df["MUNICIPALITY"] == municipality].copy()
 
     def re_arrange(v):
         if v in ["0-9", "10-19"]:
@@ -106,85 +104,53 @@ def crunch_day(df: pd.DataFrame, municipality: str) -> DailyResults:
             return "60-79"
         return "80+"
 
-    mdf["AGE_CD"] = mdf.apply(lambda row: re_arrange(row["AGE_CD"]), axis=1)
+    last_date = pd.Timestamp(sorted(df["DATE"].unique(), reverse=True)[0])
+    df = df[df["DATE"] == last_date]
+    df["AGE_CD"] = df.apply(lambda row: re_arrange(row["AGE_CD"]), axis=1)
     # print(df)
-    df_ages = mdf.groupby("AGE_CD", as_index=False).agg({
+    df_ages = df.groupby("AGE_CD", as_index=False).agg({
         'POPULATION_NBR': sum,
         'VACCINATED_FIRST_DOSIS_NBR': sum,
         'VACCINATED_SECOND_DOSIS_NBR': sum
     })
     df_ages = df_ages.sort_values(by='AGE_CD', ascending=False)
-    df_adults = mdf[mdf["ADULT_FL(18+)"] == 1]
+    population = df_ages['POPULATION_NBR'].values.tolist()
+    first_dose = df_ages['VACCINATED_FIRST_DOSIS_NBR'].values.tolist()
+    second_dose = df_ages['VACCINATED_SECOND_DOSIS_NBR'].values.tolist()
 
-    return DailyResults(
-        history_all=History(
-            population=int(mdf["POPULATION_NBR"].fillna(0).sum()),
-            total_first_dose=int(mdf["VACCINATED_FIRST_DOSIS_NBR"].fillna(0).sum()),
-            total_second_dose=int(mdf["VACCINATED_SECOND_DOSIS_NBR"].fillna(0).sum()),
-        ),
-        history_adults=History(
-            population=int(df_adults["POPULATION_NBR"].fillna(0).sum()),
-            total_first_dose=int(df_adults["VACCINATED_FIRST_DOSIS_NBR"].fillna(0).sum()),
-            total_second_dose=int(df_adults["VACCINATED_SECOND_DOSIS_NBR"].fillna(0).sum()),
-        ),
-        per_age_population=df_ages['POPULATION_NBR'].values.tolist(),
-        per_age_first_dose=df_ages['VACCINATED_FIRST_DOSIS_NBR'].values.tolist(),
-        per_age_second_dose=df_ages['VACCINATED_SECOND_DOSIS_NBR'].values.tolist()
-    )
+    return {
+        "population": population,
+        "first_dose": first_dose,
+        "second_dose": second_dose,
+        "percentage_first_dose": [round(100*v/population[i], 2) for i, v in enumerate(first_dose)],
+        "percentage_second_dose": [round(100*v/population[i], 2) for i, v in enumerate(second_dose)]
+    }
 
 
 def crunch(df: pd.DataFrame, start_date: date, end_date: date, municipality: str) -> Any:
     """."""
     date_range = pd.date_range(start=start_date, end=end_date).tolist()
     labels = [f"{d:%d-%m}" for d in date_range]
-    results = {}
-    last_day = crunch_day(df[df["DATE"] == pd.Timestamp(end_date)], municipality)
     mdf = df[df["MUNICIPALITY"] == municipality]
 
-    def timeseries(_df, population: int):
-        grouped = _df.groupby("DATE", as_index=False).agg({
-            "POPULATION_NBR": sum,
-            "VACCINATED_FIRST_DOSIS_NBR": sum,
-            "VACCINATED_SECOND_DOSIS_NBR": sum
-        }).sort_values(by="DATE", ascending=True)
-        grouped["VACCINATED_ONE_DOSIS_NBR"] = grouped.apply(
-            lambda r: r["VACCINATED_FIRST_DOSIS_NBR"] + r["VACCINATED_SECOND_DOSIS_NBR"], axis=1)
-
-        return {
-            "timeseries_minimum_one_dose": [0] + grouped["VACCINATED_ONE_DOSIS_NBR"].apply(
-                lambda v: round(v / population * 100, 2)).tolist(),
-            "timeseries_second_dose": [0] + grouped["VACCINATED_SECOND_DOSIS_NBR"].apply(
-                lambda v: round(v / population * 100, 2)).tolist()
-        }
-
-    # Compute timeseries
-    # Historical numbers (all & adults only)
-    results["history_all"] = {
-        "labels": labels,
-        "population": last_day.history_all.population,
-        "minimum_one_dose": last_day.history_all.total_minimum_one_dose,
-        "second_dose": last_day.history_all.total_second_dose,
-        **timeseries(mdf, last_day.history_all.population)
+    return {
+        # Timeseries: historical numbers, all ages
+        "history_all": {
+            "labels": labels,
+            **crunch_history(mdf)
+        },
+        # Timeseries: historical numbers for adults (18+)
+        "history_adults": {
+            "labels": labels,
+            **crunch_history(mdf[mdf["ADULT_FL(18+)"] == 1])
+        },
+        # Numbers per age
+        "per_age": {
+            "labels": ["80+", "60-79", "40-59", "20-39", "0-19"],
+            **crunch_per_age(mdf.copy())
+        },
+        "last_date": f"{df.last_date:%d/%m/%Y}"
     }
-    results["history_adults"] = {
-        "labels": labels,
-        "population": last_day.history_adults.population,
-        "minimum_one_dose": last_day.history_adults.total_minimum_one_dose,
-        "second_dose": last_day.history_adults.total_second_dose,
-        **timeseries(mdf[mdf["ADULT_FL(18+)"] == 1], last_day.history_adults.population)
-    }
-
-    # Per age numbers
-    results["per_age"] = {
-        "labels": ["80+", "60-79", "40-59", "20-39", "0-19"],
-        "population": last_day.per_age_population,
-        "first_dose": last_day.per_age_first_dose,
-        "second_dose": last_day.per_age_second_dose,
-        "percentage_first_dose": last_day.per_age_percentage_first_dose,
-        "percentage_second_dose": last_day.per_age_percentage_second_dose,
-    }
-    results["last_date"] = f"{df.last_date:%d/%m/%Y}"
-    return results
 
 
 cli = typer.Typer()
@@ -203,17 +169,19 @@ def do_fetch(date_to_fetch: str = typer.Argument(...)) -> None:
 @cli.command(name="crunch")
 def do_crunch() -> None:
     """Compute timeseries & store results to a JSON file."""
-    typer.echo("Hallo")
     # _start_date = date(2021, 1, 11)
     _start_date = date(2021, 2, 24)
     _end_date = date.today() - timedelta(days=1)
     print(f"Loading data from {_start_date} to {_end_date}")
     df = load_range(_start_date, _end_date)
     print(f"Crunch daily numbers")
-    data = crunch(df, _start_date, _end_date, "Lommel")
+    municipality = "Lommel"
+    # municipality = "Bree"
+    # municipality = "Hasselt"
+    data = crunch(df, _start_date, _end_date, municipality)
     print("Store JSON")
-    # print(data)
-    json.dump(data, open(json_path(), "w"), indent=4)
+    print(data)
+    json.dump(data, open(json_path(municipality), "w"), indent=4)
 
 
 if __name__ == "__main__":
